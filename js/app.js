9323c1b52f881ae8b1ecb9f2c08df28ccfa3cf1c
n8kolenberg <nkolenberg@gmail.com>
2015-06-06 16:24:38 +0100

    Add sound, cool gif and background music when entering cool pose

--- js/app.js
+++ js/app.js
@@ -1,44 +1,72 @@
-$('document').ready( function () {
+$(document).ready( function () {
 
-// $('.ryu').mouseenter(function(){
+$('.ryu').mouseenter(function(){
+	$('.ryu-all').hide();
+	$('.ryu-ready').show();
+})
+.mouseleave(function(){
+	$('.ryu-all').hide();
+	$('.ryu-still').show();
+});
+
+
+// $('.ryu').hover( function(){
 // 	$('.ryu-still').hide();
 // 	$('.ryu-ready').show();
-// })
-// .mouseleave(function(){
+// }, function(){
 // 	$('.ryu-still').show();
 // 	$('.ryu-ready').hide();
-// });
-
-
-
-$('.ryu').hover( function(){
-	$('.ryu-still').hide();
-	$('.ryu-ready').show();
-}, function(){
-	$('.ryu-still').show();
-	$('.ryu-ready').hide();
-}); //End hover
+// }); //End hover
 
 
 $('.ryu').mousedown(function(){
-	$('.ryu-ready').hide();
+	playHadouken();
+	$('.ryu-all').hide();
 	//ryu throws hadouken next
 	$('.ryu-throwing').show(); 
-	//play sound
-	$('.hadouken').finish().show().animate({left : "+=720px"}, 500, function(){
-		$(this).hide();
-		$(this).css({left : "520px"});
+	$('.hadouken').finish().show()
+			      .animate({left : "+=720px"}, 500, function(){
+						$(this).stop();
+						$(this).hide();
+						$(this).css({left : "520px"});
 	}); //End hadouken animate
-
-	//animate hadouken to right of screen
-	console.log("Mousedown");
 }) //End mousedown 
 
 .mouseup(function(){
-	$('.ryu-ready').show();
-	//ryu goes back to ready position
-	$('.ryu-throwing').hide();
-	console.log("Mouseup");
+	$('.ryu-all').hide();
+	$('.ryu-still').show();
 })	//End mouseup
 
 }); //End ready
+
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume =0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
+
+
+function playCool() {
+	$('#cool')[0].volume=0.5;
+	// $('#cool')[0].load();
+	$('#cool')[0].play();
+}
+
+//Cool pose
+$(document).keydown(function(event){
+	if(event.which === 88) {
+		$('.ryu-all').hide();
+		playCool();
+		$('.ryu-cool').show();
+	}
+})
+.keyup(function(event){
+	if(event.which === 88) {
+		$('#cool')[0].pause;
+		$('#cool')[0].load();
+		$('.ryu-all').hide();
+		$('.ryu-still').show();
+		
+	}
+});
